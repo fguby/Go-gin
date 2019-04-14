b9abd759d458793ea6e6b324fd00024646181536
fguby <541208156@qq.com>
2019-04-14 10:54:38 +0800

    ini配置文件

--- pkg/util/ini.go
+++ pkg/util/ini.go
@@ -2,23 +2,49 @@ package util
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/go-ini/ini"
 )
 
-var cfg, ini_err = ini.Load("conf/app.ini")
+var cfg *ini.File
+
+type Server struct {
+	App          string
+	Port         int
+	RunMode      string
+	ReadTimeOut  time.Duration
+	WriteTimeOut time.Duration
+}
+
+var ServerSetting = &Server{}
 
 func IniInit() {
-	if ini_err != nil {
-		fmt.Println("加载文件失败: %s", ini_err)
+	var err error
+	cfg, err = ini.Load("conf/app.ini")
+	if err != nil {
+		fmt.Println("加载文件失败: %s", err)
 		//非正常运行，退出整个程序
 		os.Exit(1)
 	}
+	mapTo("server", ServerSetting)
+	//换算成秒数
+	ServerSetting.ReadTimeOut = ServerSetting.ReadTimeOut * time.Second
+	ServerSetting.WriteTimeOut = ServerSetting.ReadTimeOut * time.Second
 }
 
 //根据配置文件获取属性值
 func GetIniValue(mode string, key string) string {
 	value := cfg.Section(mode).Key(key).String()
 	return value
+}
+
+// mapTo map section
+func mapTo(section string, v interface{}) {
+	err := cfg.Section(section).MapTo(v)
+	if err != nil {
+		log.Fatalf("Cfg.MapTo RedisSetting err: %v", err)
+	}
 }
