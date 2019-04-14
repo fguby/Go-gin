2e72a1e79f250b12952bdb536751ef754ed34f2d
fguby <541208156@qq.com>
2019-04-14 10:54:42 +0800

    分页类

--- pkg/util/pageutil.go
+++ pkg/util/pageutil.go
@@ -1,1 +1,20 @@
 package util
+
+type Page struct {
+	PageNo     int
+	PageSize   int
+	TotalPage  int
+	TotalCount int
+	FirstPage  bool
+	LastPage   bool
+	Data       interface{}
+}
+
+//封装分页方法
+func GetPage(count int, pageNo int, pageSize int, list interface{}) Page {
+	tp := count / pageSize
+	if count%pageSize > 0 {
+		tp = count/pageSize + 1
+	}
+	return Page{PageNo: pageNo, PageSize: pageSize, TotalPage: tp, TotalCount: count, FirstPage: pageNo == 1, LastPage: pageNo == tp, Data: list}
+}
