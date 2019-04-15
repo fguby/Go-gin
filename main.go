24d00ac9bc67137509b2b09500aa6807dbe2b0b4
fguby <541208156@qq.com>
2019-04-15 21:04:36 +0800

    添加优雅重启Http服务功能

--- main.go
+++ main.go
@@ -3,22 +3,50 @@ package main
 import (
 	_ "Gin_demo/init"
 	"Gin_demo/pkg/util"
-	"fmt"
-	"net/http"
-
 	"Gin_demo/router"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	router := router.InitRouter()
 
+	port := fmt.Sprintf(":%d", util.ServerSetting.Port)
+
 	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", util.ServerSetting.Port),
+		Addr:           port,
 		Handler:        router,
 		ReadTimeout:    util.ServerSetting.ReadTimeOut,
 		WriteTimeout:   util.ServerSetting.WriteTimeOut,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen失败:", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
