2d94504e9cd85aa0a6ebb703fdd947a9202aa2c1
fguby <541208156@qq.com>
2019-04-15 21:08:00 +0800

    路由重新编写

--- router/router.go
+++ router/router.go
@@ -1,8 +1,11 @@
 package router
 
 import (
+	jwt "Gin_demo/middleware"
 	"Gin_demo/pkg/util"
+	"Gin_demo/router/api"
 	v1 "Gin_demo/router/api/v1"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,11 +20,22 @@ func InitRouter() *gin.Engine {
 	gin.SetMode(util.ServerSetting.RunMode)
 
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		apiv1.GET("/tags", v1.GetTags)
 		apiv1.POST("/tags", v1.AddTag)
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 	}
+	//配置页面返回
+	r.LoadHTMLGlob("views/*")
+	//配置静态文件路径
+	r.Static("/static", "./static")
+	//配置首页
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "首页"})
+	})
+	//配置获取token的路径
+	r.GET("/auth", api.GetAuth)
 	return r
 }
